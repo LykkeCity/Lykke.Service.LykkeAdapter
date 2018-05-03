91c0efe84c8da01e8ca76435bef614ba5ecb13cb
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-05-03 11:45:42 +0300

    Group orders by price and sum up volumes

--- src/Lykke.Service.LykkeAdapter.Core/Filters/RepeatingOrdersFilter.cs
+++ src/Lykke.Service.LykkeAdapter.Core/Filters/RepeatingOrdersFilter.cs
@@ -1,5 +1,4 @@
 ﻿using Lykke.Service.LykkeAdapter.Core.Domain.OrderBooks;
-using MoreLinq;
 using System.Linq;
 
 namespace Lykke.Service.LykkeAdapter.Core.Filters
@@ -8,11 +7,8 @@ namespace Lykke.Service.LykkeAdapter.Core.Filters
     {
         public void FilterOutDuplicatedOrders(LykkeOrderBook orderbook)
         {
-            orderbook.Prices = orderbook.Prices.DistinctBy(x => new
-            {
-                x.Price,
-                x.Volume
-            }).ToList();
+            orderbook.Prices = orderbook.Prices.GroupBy(d => d.Price)
+                                                .Select(g => new PriceVolume(g.Key, g.Sum(s => s.Volume))).ToList();
         }
     }
 }
